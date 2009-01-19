9efcc245f519649aa50bbc5b109d139650759b80
Tim Henderson <hendersont@masran.(none)>
2009-01-18 19:36:47 -0500

    Removed example stored procedures. If you need more examples just let me know I have plenty.
    Tim Henderson <tim.tadh@gmail.com>

--- stored_procedures/update_status.sql
+++ stored_procedures/update_status.sql
@@ -8,7 +8,7 @@ CREATE PROCEDURE update_status(IN usrid VARCHAR(64), IN status_text VARCHAR(500)
 BEGIN
     UPDATE users
     SET status = status_text
-    WHERE user_id = usrid;
+    WHERE usr_id = usrid;
 END
 $$
 
