04c2bc165aa0661c223d6006a453642c8a3ccbcf
Tim Henderson <hendersont@masran.(none)>
2009-02-24 01:53:35 -0500

    Added the print_table function
    Signed-off-by: Tim Henderson <hendersont@masran.(none)>

--- templater.py
+++ templater.py
@@ -1,4 +1,4 @@
-import os, re, yaptu
+import os, re, yaptu, cgi
 
 __rex = re.compile('\s*\<\%([^\<\%]+)\%\>')
 __rbe = re.compile('\s*\<\+')
@@ -23,6 +23,30 @@ def print_template(template_path, namespace):
     cop = yaptu.copier(__rex, namespace, __rbe, __ren, __rco)
     cop.copy(s)
 
+def print_table(table, table_info, target_page=None, table_name=None, paging=False):
+    '''
+    Prints a paged html table to the standard out.
+        table = a list of lists, tuples, or dicts
+        table_info = a tuple or list of the this form
+            ((0, "column1"), (3, "column2"), (5, "column3"))
+            the numbers refer to the column in the table variable the strings are the name of the
+            columns
+        target_page = The page the table resides on (needed if paging=True)
+        table_name = The name of the table (it doesn't print out just used by the pager to
+                     refer to this table, need if paging=True)
+        paging = turns paging on or off
+    '''
+    form = cgi.FieldStorage()
+    if form.has_key(table_name+'_page'): page = int(form[table_name+'_page'].value)
+    else: page = 0
+    
+    if paging: rows_per_page = 15
+    else: rows_per_page = 0
+    
+    if (len(table)%rows_per_page) == 0: pages = len(table)/rows_per_page
+    else: pages = len(table)/rows_per_page + 1
+    
+    print_template("templates/create_table.html", locals())
 
 if __name__ == '__main__':
     print locals()
--- user_list.py
+++ user_list.py
@@ -4,17 +4,29 @@ import config
 import os, re, cgi, templater, db
 import cookie_session, user_manager
 
-form = cgi.FieldStorage()
-ses_dict, user_dict = user_manager.init_user_session(form)
 
-if user_dict == {}:
-    target_page = 'user_list.py'
-    templater.print_template("templates/login_template.html", locals())
-else:
+def print_user_list():
     con = db.connections.get_con()
     cur = db.DictCursor(con)
     cur.callproc('users_table')
     users = cur.fetchall()
     cur.close()
     db.connections.release_con(con)
+    
+    table_info = ((0, "name"), (1, "screen name"), (2, "email"), (3, ""))
+    table = list()
+    for row in users:
+        send_msg = "<a class='inline' href='send_msg.py?sn="+row['screen_name']+"'>message</a>"
+        table.append((row['name'], row['screen_name'], row['email'], send_msg))
     templater.print_template("templates/user_list.html", locals())
+
+if __name__ == '__main__':
+    
+    form = cgi.FieldStorage()
+    ses_dict, user_dict = user_manager.init_user_session(form)
+    
+    if user_dict == {}:
+        target_page = 'user_list.py'
+        templater.print_template("templates/login_template.html", locals())
+    else:
+        print_user_list()
