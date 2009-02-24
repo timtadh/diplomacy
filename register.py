e4cb4f5165b64a7d9ff6abec181647d2720f428a
Tim Henderson <hendersont@masran.(none)>
2009-02-24 03:06:36 -0500

    improved how the validation is done in register.py
    Signed-off-by: Tim Henderson <hendersont@masran.(none)>

--- register.py
+++ register.py
@@ -4,6 +4,7 @@ import config
 import os, re, cgi, templater, db
 import cookie_session, user_manager
 import sys
+import formencode
 from formencode import validators
     
 
@@ -12,18 +13,31 @@ ses_dict, user_dict = user_manager.init_user_session(form)
 
 if user_dict == {}:
     
-    
-    
     if form.has_key("name") and form.has_key("screen_name") and form.has_key("email") and\
        form.has_key("passwd1") and form.has_key("passwd2"):
         try:
             email = validators.Email(resolve_domain=True, not_empty=True).to_python(form["email"].value)
+        except formencode.Invalid, e:
+            templater.print_error("email: "+str(e))
+            sys.exit()
+        
+        try:
             name = validators.PlainText(r"^[a-zA-Z_\-0-9 ]*$", not_empty=True).to_python(form["name"].value)
+        except formencode.Invalid, e:
+            templater.print_error("name: "+str(e))
+            sys.exit()
+        
+        try:
             screen_name = validators.PlainText(not_empty=True).to_python(form["screen_name"].value)
+        except formencode.Invalid, e:
+            templater.print_error("screen_name: "+str(e))
+            sys.exit()
+        
+        try:
             ps_validators = validators.FieldsMatch('p1', 'p2')
             ps = ps_validators.to_python({'p1':form["passwd1"].value, 'p2':form["passwd2"].value})['p1']
         except formencode.Invalid, e:
-            templater.print_error(repr(e))
+            templater.print_error("Passwords: "+str(e))
             sys.exit()
         
         if user_manager.get_user_byemail(form["email"].value):
@@ -34,6 +48,9 @@ if user_dict == {}:
         user_manager.add_user(user_manager.gen_userID(), name, email,
                               screen_name, ps)
         templater.print_template("templates/login_template.html", {'target_page':'main.py'})
+    elif form.has_key("name") or form.has_key("screen_name") or form.has_key("email") or\
+         form.has_key("passwd1") or form.has_key("passwd2"):
+        templater.print_error("All fields must be filled out.")
     else:
         templater.print_template("templates/register.html", {'target_page':'register.py'})
 else:
