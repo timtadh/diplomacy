dc2d74ac58363a79d034a04c4dcf3c437709c349
Steve Johnson <johnsons@masran.(none)>
2009-03-17 17:30:28 -0400

    Tweaking main page to contain useful information. Not finished.
    Steve Johnson <srj15@case.edu>

--- main.py
+++ main.py
@@ -7,8 +7,18 @@ import cookie_session, user_manager
 form = cgi.FieldStorage()
 ses_dict, user_dict = user_manager.init_user_session(form)
 
-if user_dict == {}:
-    target_page = 'main.py'
-    templater.print_template("templates/login_template.html", locals())
-else:
+def print_main(user_dict):
+    num_users = 9001
+    num_online = 9001
+    num_games_total = 9001
+    num_games_active = 9001
+    num_games_usr = 9001
+    unread_msgs = 9001
     templater.print_template("templates/main.html", locals())
+
+if __name__ == "__main__":
+    if user_dict == {}:
+        target_page = 'main.py'
+        templater.print_template("templates/login_template.html", locals())
+    else:
+        print_main(user_dict)
