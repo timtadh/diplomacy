a222be166e2698e10e059efb537d35bf48d78b6a
Leigh Praskac <praskacl@masran.(none)>
2009-03-26 22:48:46 -0400

    This is the page that tells what country currently owns what supply center during a game.
    Leigh Praskac <leigh.praskac@case.edu>

--- supply_count.py
+++ supply_count.py
@@ -4,18 +4,27 @@ import config
 import os, re, cgi, templater, db
 import cookie_session, user_manager
 
-def print_supplycenters(user_dict):
-    con = db.connections.get_con()
-    cur = db.DictCursor(con)
-    cur.callproc('supply_count', ses_dict['gam_id'])
-    suppliers = cur.fetchall()
-    cur.close()
-    db.connections.release_con(con)
+def check_switch(switch, ng):
+    if switch:
+        con = db.connections.get_con()
+        cur = db.DictCursor(con)
+        cur.callproc('set_session_gam_id', (ses_dict['session_id'], user_dict['usr_id'], ng))
+        db.connections.release_con(con)
+        update_ses_dict()
 
-    table_info = (("tname", "Territory"), ("cname", "Occupier"))
-    table = suppliers
-	
-    templater.print_template("templates/supply_count.html", locals())
+def get_table(con, name, args):
+    cur = db.DictCursor(con)
+    cur.callproc(name, args)
+    table = cur.fetchall()
+    cur.close()
+    return table
+
+def print_supplycenters(user_dict):
+    supplier_table_info = (('abbrev', "Abbrev"), ('name', "Name"))  
+    supplier_table = get_table(
+        con, 'usr_suppliers_in_game', (ses_dict['gam_id'], user_dict['usr_id'])
+    )
+    return supplier_table, supplier_table_info
 
 if __name__ == '__main__':
     
