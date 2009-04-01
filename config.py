d557ed3245a38a65ba78201c66a757ac48c697b9
Tim Henderson <tim.tadh@gmail.com>
2009-04-01 15:29:45 -0400

    moved to using twiks configurator
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- config.py
+++ config.py
@@ -1,9 +1,4 @@
-import sys
-sys.stderr = sys.stdout
-import cgitb; cgitb.enable()
-import config_db_con
-import warnings
-warnings.simplefilter('ignore', UserWarning)
+import twik.default_config
 import twik.db as db
 import mapgen.db as db2
 for attr in dir(db):
